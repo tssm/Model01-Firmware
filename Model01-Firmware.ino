991dfc0c15a3f7bc327f48695ab45228b7eb79b1
Tae Sandoval <tssm@users.noreply.github.com>
2019-03-25 11:14:25 -0300

    Swipe ' and = keys

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -224,8 +224,8 @@ KEYMAPS(
    Key_CapsLock,
 
    Key_Backspace,    Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Minus,
-   Key_RightControl, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Quote,
-                     Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Equals,
+   Key_RightControl, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
+                     Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_RightShift,   Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_RightBracket,
    ShiftToLayer(FUNCTION),
    Key_RightAlt,
